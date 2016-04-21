d92dbed3bc87f347c039a199679d8e941b00e508
Carl Schroedl <cschroedl@usgs.gov>
2016-04-21 11:15:20 -0500

    Decompressing table before altering, recompressing after.

--- sparrow-liquibase/src/main/resources/liquibase/alterAndUpdateSparrowModel16/alterSparrowModel16.sql
+++ sparrow-liquibase/src/main/resources/liquibase/alterAndUpdateSparrowModel16/alterSparrowModel16.sql
@@ -5,6 +5,7 @@
 --logicalFilePath: alterSparrowModel.sql
 
 --changeset cschroed:addColumnsToSparrowModel
+alter table SPARROW_MODEL move NOCOMPRESS;
 alter table SPARROW_MODEL add (
 	states nvarchar2(300),
 	is_national char(1) DEFAULT 'F',
@@ -12,4 +13,6 @@ alter table SPARROW_MODEL add (
 	base_year number
 )
 
---rollback alter table SPARROW_MODEL drop (states, national, regions, base_year);
+alter table SPARROW_MODEL move COMPRESS;
+
+--rollback alter table SPARROW_MODEL drop (states, national, regions, base_year); alter table SPARROW_MODEL move COMPRESS;
